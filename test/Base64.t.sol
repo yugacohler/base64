9d5ec6c4e540424c3c3966cc08d34aa1ab08f181
Yuga Cohler <yuga.cohler@coinbase.com>
2023-08-17 10:19:21 -0400

    Add stub Base64 contract and test

---
+++ test/Base64.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "../lib/forge-std/src/Test.sol";
+
+// Unit tests for Base64.
+contract Base64Test is Test {
+
+  function setUp() public {}
+
+  function testNull() public {}
+}
