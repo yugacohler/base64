3171552277d685366c62d6370959451baed73f3f
Yuga Cohler <yuga.cohler@coinbase.com>
2023-08-17 10:39:02 -0400

    Two tests passing

--- src/Base64.sol
+++ src/Base64.sol
@@ -6,8 +6,42 @@ import {Owned} from "../lib/solmate/src/auth/Owned.sol";
 
 // Base64, a Smart Contract for Tournament-based pools.
 contract Base64 is IBase64, Owned {
+  ////////// MEMBER VARIABLES //////////
+
+  // The mapping from team ID to team.
+  mapping(uint256 => Team) public teams;
+
+  // The current bracket.
+  uint256[][] public bracket;
+
   ////////// CONSTRUCTOR //////////
-  constructor() Owned(msg.sender) {}
+
+  // Initializes the Base64 bracket with the given teams.
+  // The number of teams must be a power of two between 4 and 256 inclusive.
+  constructor(uint32[] memory teamIDs, string[] memory teamNames) Owned(msg.sender) {
+    require(isPowerOfTwo(teamIDs.length), "INVALID_BRACKET_SIZE");
+    require(teamIDs.length == teamNames.length, "INVALID_TEAM_DATA");
+    require(checkTeamIDs(teamIDs), "DUPLICATE_TEAM_IDS");
+
+    // Initialize the teams.
+    for (uint256 i = 0; i < teamNames.length; i++) {
+      teams[teamIDs[i]] = Team(teamIDs[i], teamNames[i]);
+    }
+
+    // Initialize the bracket.
+    uint256 teamsLeft = teamNames.length;
+
+    while (teamsLeft >= 1) {
+      uint256[] memory round = new uint256[](teamsLeft);
+      bracket.push(round);
+      teamsLeft /= 2;
+    }
+
+    // Initialize the first round of the bracket.
+    for (uint256 i = 0; i < teamNames.length; i++) {
+      bracket[0][i] = teamIDs[i];
+    }    
+  }
 
   ////////// PUBLIC APIS //////////
 
@@ -37,5 +71,25 @@ contract Base64 is IBase64, Owned {
   
   function collectPayout() override external {
     require(false, "NOT_IMPLEMENTED");
+  }
+
+  ////////// PRIVATE HELPERS //////////
+
+  // Returns true if a number is a power of 2 greater than 4 and less than 256.
+  function isPowerOfTwo(uint256 x) private pure returns (bool) {
+      return x >= 4 && x <= 256 && (x & (x - 1)) == 0;
+  }
+
+  // Returns true if the team IDs are unique.
+  function checkTeamIDs(uint32[] memory teamIDs) private pure returns (bool) {
+      for (uint8 i = 0; i < teamIDs.length; i++) {
+          for (uint8 j = i + 1; j < teamIDs.length; j++) {
+              if (teamIDs[i] == teamIDs[j]) {
+                  return false;
+              }
+          }
+      }
+
+      return true;
   }
 }
--- test/Base64.t.sol
+++ test/Base64.t.sol
@@ -1,12 +1,65 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {Base64} from "../src/Base64.sol";
 import {Test} from "../lib/forge-std/src/Test.sol";
 
 // Unit tests for Base64.
 contract Base64Test is Test {
+  Base64 b;
 
-  function setUp() public {}
+  function setUp() public {
+    uint32[] memory teamIDs = new uint32[](8);
+    for (uint8 i = 0; i < teamIDs.length; i++) {
+      teamIDs[i] = i;
+    }
 
-  function testNull() public {}
+    string[] memory teamNames = new string[](8);
+    teamNames[0] = "Brian";
+    teamNames[1] = "Greg";
+    teamNames[2] = "Alesia";
+    teamNames[3] = "Manish";
+    teamNames[4] = "LJ";
+    teamNames[5] = "Paul";
+    teamNames[6] = "Emilie";
+    teamNames[7] = "Will";
+
+    b = new Base64(teamIDs, teamNames);
+  }
+
+  function testConstructor_tooShort() public {
+    uint32[] memory invalidTeamIDs = new uint32[](3);
+    for (uint8 i = 0; i < invalidTeamIDs.length; i++) {
+      invalidTeamIDs[i] = i;
+    }
+
+    string[] memory invalidTeamNames = new string[](3);
+    invalidTeamNames[0] = "Brian";
+    invalidTeamNames[1] = "Greg";
+    invalidTeamNames[2] = "Alesia";
+
+    vm.expectRevert("INVALID_BRACKET_SIZE");
+
+    new Base64(invalidTeamIDs, invalidTeamNames);
+  }
+
+  function testConstructor_notPowerOfTwo() public {
+    uint32[] memory invalidTeamIDs = new uint32[](5);
+    for (uint8 i = 0; i < invalidTeamIDs.length; i++) {
+      invalidTeamIDs[i] = i;
+    }
+
+    string[] memory invalidTeamNames = new string[](5);
+    invalidTeamNames[0] = "Brian";
+    invalidTeamNames[1] = "Greg";
+    invalidTeamNames[2] = "Alesia";
+    invalidTeamNames[3] = "Manish";
+    invalidTeamNames[4] = "LJ";
+
+    vm.expectRevert("INVALID_BRACKET_SIZE");
+
+    new Base64(invalidTeamIDs, invalidTeamNames);
+  }
+
+
 }
