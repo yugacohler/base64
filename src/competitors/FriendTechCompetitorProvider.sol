a6e378dfa93db50f1ac4e53a105f69ef5cf05285
Yuga Cohler <yuga.cohler@coinbase.com>
2023-08-21 21:36:02 -0400

    Done with FriendTechCompetitorProvider

--- src/competitors/FriendTechCompetitorProvider.sol
+++ src/competitors/FriendTechCompetitorProvider.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.13;
 import {CompetitorProvider} from "../CompetitorProvider.sol";
 import {ERC721} from "../../lib/solmate/src/tokens/ERC721.sol";
 import {IBase64} from "../IBase64.sol";
+import {Strings} from "../../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
 // A competitor provider for FriendTech competitors.
 contract FriendTechCompetitorProvider is CompetitorProvider {
+  ////////// CONSTANTS //////////
+
+  // The base URI for FriendTech metadata.
+  string public constant BASE_URI = "https://prod-api.kosetto.com/users/";
+
   ////////// MEMBER VARIABLES //////////
 
   // The map from FriendTech ID to FriendTech wallet address.
@@ -14,18 +20,17 @@ contract FriendTechCompetitorProvider is CompetitorProvider {
 
   ////////// CONSTRUCTOR //////////
 
-  constructor(uint256[] memory _ids, address[] memory _addresses) {
-    require(_ids.length == _addresses.length, "INVALID_NUM_ADDRESSES");
-
-    uint256 powerOfTwo = getPowerOfTwo(_ids.length);
+  constructor(address[] memory _addresses) {
+    uint256 powerOfTwo = getPowerOfTwo(_addresses.length);
 
     for (uint16 i = 0; i < powerOfTwo; i++) {
-      require(_ids[i] != 0, "ZERO_ID");
-      ids.push(_ids[i]);
+      require(_addresses[i] != address(0), "ZERO_ID");
+      uint256 id = uint256(uint160(_addresses[i]));
+      ids.push(id);
 
       // Fail if there was a duplicate ID.
-      require(addresses[_ids[i]] == address(0), "DUPLICATE_IDS");
-      addresses[_ids[i]] = _addresses[i];
+      require(addresses[id] == address(0), "DUPLICATE_IDS");
+      addresses[id] = _addresses[i];
     }
   }
   
@@ -40,7 +45,7 @@ contract FriendTechCompetitorProvider is CompetitorProvider {
 
     return IBase64.Competitor({
       id: competitorId,
-      uri: "" // TODO
+      uri: string(abi.encodePacked(BASE_URI, Strings.toHexString(addresses[competitorId])))
     });
   }
 }
