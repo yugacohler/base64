7cb316d4bf09d45ddbf5f87e16887473777791eb
Yuga Cohler <yuga.cohler@coinbase.com>
2023-08-22 13:24:15 -0400

    Refactor

--- script/Base64.s.sol
+++ script/Base64.s.sol
@@ -5,30 +5,30 @@ import {Script} from "../lib/forge-std/src/Script.sol";
 // import {Base64} from "../src/Base64.sol";
 
 contract Base64Script is Script {
-    uint32[] teamIDs;
-    string[] teamNames;
+    uint32[] _teamIDs;
+    string[] _teamNames;
 
     function setUp() public {
-        teamIDs = new uint32[](8);
-        for (uint8 i = 0; i < teamIDs.length; i++) {
-            teamIDs[i] = i + 1;
+        _teamIDs = new uint32[](8);
+        for (uint8 i = 0; i < _teamIDs.length; i++) {
+            _teamIDs[i] = i + 1;
         }
 
-        teamNames = new string[](8);
-        teamNames[0] = "Brian";
-        teamNames[1] = "Greg";
-        teamNames[2] = "Alesia";
-        teamNames[3] = "Manish";
-        teamNames[4] = "LJ";
-        teamNames[5] = "Paul";
-        teamNames[6] = "Emilie";
-        teamNames[7] = "Will";
+        _teamNames = new string[](8);
+        _teamNames[0] = "Brian";
+        _teamNames[1] = "Greg";
+        _teamNames[2] = "Alesia";
+        _teamNames[3] = "Manish";
+        _teamNames[4] = "LJ";
+        _teamNames[5] = "Paul";
+        _teamNames[6] = "Emilie";
+        _teamNames[7] = "Will";
     }
 
     function run() public {
         vm.startBroadcast();
 
-        // new Base64(teamIDs, teamNames);
+        // new Base64(_teamIDs, _teamNames);
 
         vm.stopBroadcast();
     }
